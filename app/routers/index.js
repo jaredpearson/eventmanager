afa4e4640310e9733f74ed4694aae0f51f6ddfab
Jared Pearson <jaredapearson@gmail.com>
2015-09-03 21:32:56 -0700

    Added the home route and page

--- app/routers/index.js
+++ app/routers/index.js
@@ -4,5 +4,6 @@ var router = require('express').Router();
 
 router.use(require('./default').router);
 router.use(require('./login').router);
+router.use(require('./home').router);
 
 module.exports.router = router;
