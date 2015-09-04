9f14d1c9e66b6664589261486974d9dd932b2c32
Jared Pearson <jaredapearson@gmail.com>
2015-09-04 09:37:14 -0700

    Added upcoming events to home page

--- app/data_sources/events.js
+++ app/data_sources/events.js
@@ -3,6 +3,35 @@
 var db = require('../db'),
     Q = require('q');
 
+function isInt(value) {
+    if (isNaN(value)) {
+        return false;
+    }
+    var x = parseFloat(value);
+    return (x | 0) === x;
+}
+
+function UserCache() {
+    this.usersById = {};
+}
+UserCache.prototype.findById = function(id) {
+    return this.usersById[id];
+}
+UserCache.prototype.put = function(id, user) {
+    if (id && user) {
+        this.usersById[id] = user;
+    }
+}
+UserCache.prototype.getOrPut = function(id, user) {
+    var found = this.findById(id);
+    if (found) {
+        return found;
+    } else {
+        this.put(id, user);
+        return user;
+    }
+}
+
 module.exports = {
     getNameMaxLength: function() {
         return 50;
@@ -28,6 +57,62 @@ module.exports = {
                 } else {
                     return Q(undefined);
                 }
+            })
+            .fin(function() {
+                client.done();
+            });
+    },
+
+    getNewestEvents: function(limit) {
+        var client,
+            limitClean;
+
+        limitClean = isInt(limit) ? limit : 10;
+
+        return db.connect()
+            .then(function(c) {
+                client = c;
+                return Q(c);
+            })
+            .then(function() {
+                return client.query('SELECT e.events_id, e.event_name, e.owner, ou.first_name owner_first_name, ou.last_name owner_last_name, e.start, e.description, e.created, e.created_by, cu.first_name created_by_first_name, cu.last_name created_by_last_name FROM Events e LEFT OUTER JOIN Users ou ON e.owner = ou.users_id LEFT OUTER JOIN Users cu ON e.created_by = cu.users_id ORDER BY e.created DESC, e.events_id LIMIT ' + limitClean);
+            })
+            .then(function(results) {
+                var events = [],
+                    userCache = new UserCache();
+
+                // transform the row in to a hierarchy of objects
+                results.rows.forEach(function(eventData) {
+                    var ownerUser,
+                        createdByUser;
+
+                    if (eventData.owner) {
+                        ownerUser = userCache.getOrPut(eventData.owner, {
+                            id: eventData.owner,
+                            firstName: eventData.owner_first_name,
+                            lastName: eventData.owner_last_name
+                        });
+                    }
+
+                    if (eventData.created_by) {
+                        createdByUser = userCache.getOrPut(eventData.created_by, {
+                            id: eventData.created_by,
+                            firstName: eventData.created_by_first_name,
+                            lastName: eventData.created_by_last_name
+                        });
+                    }
+
+                    events.push({
+                        id: eventData.events_id,
+                        name: eventData.event_name,
+                        owner: ownerUser,
+                        start: eventData.start,
+                        description: eventData.description,
+                        created: eventData.created,
+                        createdBy: createdByUser
+                    });
+                })
+                return Q(events);
             })
             .fin(function() {
                 client.done();
--- app/routers/home.js
+++ app/routers/home.js
@@ -1,10 +1,23 @@
 'use strict';
 
 var router = require('express').Router(),
-    auth = require('../middlewares/auth');
+    auth = require('../middlewares/auth'),
+    eventsDataSource = require('../data_sources/events');
 
 router.get('/home', auth, function(req, res) {
-    res.render('pages/home');
+
+    eventsDataSource.getNewestEvents()
+        .then(function(events) {
+            res.render('pages/home', {
+                events: events
+            });
+        })
+        .fail(function(err) {
+            console.error(err);
+            res.render('pages/unexpected_error');
+        })
+        .done();
+
 });
 
 module.exports.router = router;
