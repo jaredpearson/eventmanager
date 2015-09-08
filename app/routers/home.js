351d7f9a0afc86f935eca3a1bd3967a0e734d296
Jared Pearson <jaredapearson@gmail.com>
2015-09-08 10:35:27 -0700

    Added some formatting to the start of the event on home

--- app/routers/home.js
+++ app/routers/home.js
@@ -4,12 +4,28 @@ var router = require('express').Router(),
     auth = require('../middlewares/auth'),
     eventsDataSource = require('../data_sources/events');
 
+function HomeListEventModel(event) {
+    this.name = event.name;
+    this.start = event.start;
+    this.startDateFormatted = event.start.format('ddd MMM D'); // Tues Sep 8
+    this.startTimeFormatted = event.start.format('H:mm A');    // 10:33 AM
+}
+
+function convertEventsToEventModels(events) {
+    var eventModels = [];
+    events.forEach(function(event) {
+        eventModels.push(new HomeListEventModel(event));
+    });
+    return eventModels;
+}
+
 router.get('/home', auth, function(req, res) {
 
     eventsDataSource.getNewestEvents()
         .then(function(events) {
+            var eventModels = convertEventsToEventModels(events);
             res.render('pages/home', {
-                events: events
+                events: eventModels
             });
         })
         .fail(function(err) {
